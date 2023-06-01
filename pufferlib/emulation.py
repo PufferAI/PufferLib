83240e88a6e9ef1f83614bfb92f6801e4b39f2b5
David Bloomin <daveey@gmail.com>
2023-05-31 18:54:13 -0700

    fix dones handling

--- pufferlib/emulation.py
+++ pufferlib/emulation.py
@@ -302,7 +302,8 @@ def make_puffer_env_cls(scope, raw_obs):
                     del rewards[team]
 
                 if dones[team]:
-                    dones[team] = self.done or all(dones[team].values())
+                    self.num_live_players[team] -= sum(dones[team].values())
+                    dones[team] = self.done or (self.num_live_players[team] == 0)
                 elif scope.emulate_const_num_agents:
                     dones[team] = self.done
                 else:
@@ -457,6 +458,7 @@ def make_puffer_env_cls(scope, raw_obs):
             self.agents = self.env.agents
             self.initialized = True
             self.done = False
+            self.num_live_players = {tid: len(team) for tid, team in self._teams.items()}
 
             # Sort observations according to possible_agents
             # All keys must be present in possible_agents on reset
