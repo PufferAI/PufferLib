3123de33024d53ba85ec751f8ebd97a49edbf594
jsuarez5341 <sealsuarez@gmail.com>
2023-07-12 06:26:04 +0000

    Vectorization optimizations

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -24,31 +24,32 @@ def _test_vectorization_performance(binding, vec_backend, steps, num_workers, en
     )
     envs.reset()
 
+    # Warmup
+    for i in range(10):
+        envs.step(actions)
+
     start = time.perf_counter()
     for i in range(steps):
         envs.step(actions)
     total_time = time.perf_counter() - start
 
     envs.close()
-    return steps / total_time
+    return steps * num_workers * envs_per_worker / total_time
 
 
 if __name__ == '__main__':
     steps = 10000
     delays = [0, 0.0001, 0.001]
     backends = [
-        pufferlib.vectorization.Serial,
-        #pufferlib.vectorization.multiprocessing.VecEnv,
-        #pufferlib.vectorization.multiprocessing_shared.VecEnv,
-        #pufferlib.vectorization.ray.VecEnv,
-        #pufferlib.vectorization.ray_shared.VecEnv,
+        #pufferlib.vectorization.Serial,
+        pufferlib.vectorization.Multiprocessing,
     ]
 
     for d in delays:
         for b in backends:
             binding = PerformanceBinding(delay=d, bandwith=1)
             sps = _test_vectorization_performance(binding, b, steps, 1, 1)
-            print(f'{b.__module__.split(".")[-1]}: {steps} steps with {d} delay: {sps} steps per second')
+            print(f'{b.__name__.split(".")[-1]}: {steps} steps with {d} delay: {sps} steps per second')
 
     #import pufferlib.registry.atari
     #binding = pufferlib.registry.atari.make_binding('BreakoutNoFrameskip-v4', framestack=1)
