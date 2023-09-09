da820dcf1ef10856598141a17da16fe96ae05938
Joseph Suarez <sealsuarez@gmail.com>
2023-09-09 01:29:18 +0000

    Serializable policies

--- pufferlib/policy_store.py
+++ pufferlib/policy_store.py
@@ -1,25 +1,25 @@
+from pdb import set_trace as T
 from typing import Dict, Set, List, Callable
 
 import torch
 
-from pufferlib.models import Policy
 import logging
 
 import copy
 import os
 import numpy as np
 
+from pufferlib.models import Policy
+from pufferlib.pytorch import save_model, load_model
+
+
 class PolicyRecord():
-  def __init__(self, name: str, policy: Policy, policy_args = None):
+  def __init__(self, name: str, policy: Policy):
     self.name = name
-    self._policy_args = policy_args
     self._policy = policy
 
-  def policy(self) -> Policy:
+  def policy(self, policy_args=[], policy_kwargs={}, device=None) -> Policy:
     return self._policy
-
-  def policy_args(self) -> Dict:
-    return self._policy_args
 
 class PolicySelector():
   def __init__(self, num: int, exclude_names: Set[str] = None):
@@ -34,7 +34,7 @@ class PolicySelector():
     return [policies[name] for name in selected_names]
 
 class PolicyStore():
-  def add_policy(self, name: str, policy: Policy, policy_args = None) -> PolicyRecord:
+  def add_policy(self, name: str, policy: Policy) -> PolicyRecord:
     raise NotImplementedError
 
   def add_policy_copy(self, name: str, src_name: str) -> PolicyRecord:
@@ -54,25 +54,23 @@ class MemoryPolicyStore(PolicyStore):
     super().__init__()
     self._policies = dict()
 
-  def add_policy(self, name: str, policy: Policy, policy_args = None) -> PolicyRecord:
+  def add_policy(self, name: str, policy: Policy) -> PolicyRecord:
     if name in self._policies:
         raise ValueError(f"Policy with name {name} already exists")
 
-    self._policies[name] = PolicyRecord(name, policy, policy_args)
+    self._policies[name] = PolicyRecord(name, policy)
     return self._policies[name]
 
   def add_policy_copy(self, name: str, src_name: str) -> PolicyRecord:
-    return self.add_policy(
-      name,
-      copy.deepcopy(self._policies[src_name].policy()),
-      copy.deepcopy(self._policies[src_name].policy_args))
+    policy_copy = copy.deepcopy(self._policies[src_name].policy())
+    return self.add_policy(name, policy_copy)
 
   def _all_policies(self) -> List:
     return self._policies
 
 class FilePolicyRecord(PolicyRecord):
-  def __init__(self, name: str, path: str, policy: Policy = None, policy_args = None):
-    super().__init__(name, policy, policy_args)
+  def __init__(self, name: str, path: str, policy: Policy = None):
+    super().__init__(name, policy)
     self._path = path
 
   def save(self):
@@ -81,46 +79,28 @@ class FilePolicyRecord(PolicyRecord):
       raise ValueError(f"Policy {self._path} already exists")
     logging.info(f"Saving policy to {self._path}")
     temp_path = self._path + ".tmp"
-    torch.save({
-        "policy_state_dict": self._policy.state_dict(),
-        "policy_args": self._policy_args
-    }, temp_path)
-    os.rename(temp_path, self._path)
+    save_model(self._policy, path=temp_path)
+    os.rename(temp_path + '.pt', self._path + '.pt')
+    os.rename(temp_path + '.pickle', self._path + '.pickle')
 
-  def load(self, create_policy_func: Callable, envs: None, device = None):
-    data = self._load_data(device)
-    policy = create_policy_func(envs)  # CHECK ME: do we need additional args to pass in?
-    policy.load_state_dict(data["policy_state_dict"])
-    policy.is_recurrent = hasattr(policy, "lstm")
-    return policy
+  def load(self, policy_args=[], policy_kwargs={}, device=None):
+    return load_model(self._path, policy_args, policy_kwargs, map_location=device)[0]
 
-  def _load_data(self, device = None):
-    if not os.path.exists(self._path):
-      raise ValueError(f"Policy with name {self.name} does not exist")
-    data = torch.load(self._path, map_location=device)
-    self._policy_args = data["policy_args"]
-    return data
-
-  def policy_args(self) -> Dict:
-    if self._policy_args is None:
-      self._load_data()
-    return self._policy_args
-
-  def policy(self, create_policy_func: Callable = None, envs = None, device = None) -> Policy:
+  def policy(self, policy_args=[], policy_kwargs={}, device=None) -> Policy:
+    if self._policy is not None:
+      return self._policy
     if device is None:
       device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    if self._policy is None:
-      self._policy = self.load(create_policy_func, envs, device).to(device)
-    return self._policy
+    return self.load(policy_args, policy_kwargs, device)
 
 class DirectoryPolicyStore(PolicyStore):
   def __init__(self, path: str):
     self._path = path
     os.makedirs(self._path, exist_ok=True)
 
-  def add_policy(self, name: str, policy: Policy, policy_args = None) -> PolicyRecord:
-    path = os.path.join(self._path, name + ".pt")
-    pr = FilePolicyRecord(name, path, policy, policy_args)
+  def add_policy(self, name: str, policy: Policy) -> PolicyRecord:
+    path = os.path.join(self._path, name)
+    pr = FilePolicyRecord(name, path, policy)
     pr.save()
     return pr
 
@@ -132,5 +112,5 @@ class DirectoryPolicyStore(PolicyStore):
     for file in os.listdir(self._path):
       if file.endswith(".pt"):
         name = file[:-3]
-        policies[name] = FilePolicyRecord(name, os.path.join(self._path, file))
+        policies[name] = FilePolicyRecord(name, os.path.join(self._path, name))
     return policies
--- tests/test_policy_store.py
+++ tests/test_policy_store.py
@@ -1,29 +1,27 @@
 import copy
+
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import patch
+
+import torch
+
 from pufferlib.models import Policy
-
 from pufferlib.policy_store import DirectoryPolicyStore, FilePolicyRecord, MemoryPolicyStore, PolicyRecord, PolicySelector
-import unittest
-from unittest.mock import MagicMock, patch
 
-class MockPolicy:
-    pass
 
 class TestPolicyRecord(unittest.TestCase):
     def test_initialization(self):
-        pr = PolicyRecord('policy1', MockPolicy())
+        pr = PolicyRecord('policy1', torch.nn.Module())
         self.assertEqual(pr.name, 'policy1')
-        self.assertIsInstance(pr.policy(), MockPolicy)
-        self.assertEqual(pr.metadata, None)
+        self.assertIsInstance(pr.policy(), torch.nn.Module)
 
 class TestPolicySelector(unittest.TestCase):
     def test_selection(self):
         ps = PolicySelector(2)
         policies = {
-            'policy1': PolicyRecord('policy1', MockPolicy()),
-            'policy2': PolicyRecord('policy2', MockPolicy()),
-            'policy3': PolicyRecord('policy3', MockPolicy()),
+            'policy1': PolicyRecord('policy1', torch.nn.Module()),
+            'policy2': PolicyRecord('policy2', torch.nn.Module()),
+            'policy3': PolicyRecord('policy3', torch.nn.Module()),
         }
         selected_policies = ps.select_policies(policies)
         self.assertEqual(len(selected_policies), 2)
@@ -31,9 +29,9 @@ class TestPolicySelector(unittest.TestCase):
     def test_selection_with_exclusion(self):
         ps = PolicySelector(2, exclude_names={'policy2'})
         policies = {
-            'policy1': PolicyRecord('policy1', MockPolicy()),
-            'policy2': PolicyRecord('policy2', MockPolicy()),
-            'policy3': PolicyRecord('policy3', MockPolicy()),
+            'policy1': PolicyRecord('policy1', torch.nn.Module()),
+            'policy2': PolicyRecord('policy2', torch.nn.Module()),
+            'policy3': PolicyRecord('policy3', torch.nn.Module()),
         }
         selected_policies = ps.select_policies(policies)
         self.assertEqual(len(selected_policies), 2)
@@ -44,20 +42,19 @@ class TestMemoryPolicyStore(unittest.TestCase):
         self.store = MemoryPolicyStore()
 
     def test_add_policy(self):
-        policy = MockPolicy()
-        record = self.store.add_policy('policy1', policy, {'key': 'value'})
+        policy = torch.nn.Module()
+        record = self.store.add_policy('policy1', policy)
         self.assertEqual(record.name, 'policy1')
-        self.assertEqual(record.metadata, {'key': 'value'})
-        self.assertIsInstance(record.policy(), MockPolicy)
+        self.assertIsInstance(record.policy(), torch.nn.Module)
 
     def test_add_policy_error(self):
-        policy = MockPolicy()
+        policy = torch.nn.Module()
         self.store.add_policy('policy1', policy)
         with self.assertRaises(ValueError):
             self.store.add_policy('policy1', policy)
 
     def test_add_policy_copy(self):
-        policy = MockPolicy()
+        policy = torch.nn.Module()
         policy_copy = copy.deepcopy(policy)
         self.store.add_policy('policy1', policy)
         record_copy = self.store.add_policy_copy('policy2', 'policy1')
@@ -65,7 +62,7 @@ class TestMemoryPolicyStore(unittest.TestCase):
         self.assertEqual(record_copy.name, 'policy2')
 
     def test_all_policies(self):
-        policy = MockPolicy()
+        policy = torch.nn.Module()
         self.store.add_policy('policy1', policy)
         self.store.add_policy('policy2', policy)
         all_policies = self.store._all_policies()
@@ -73,42 +70,31 @@ class TestMemoryPolicyStore(unittest.TestCase):
 
     def test_select_policies(self):
         selector = PolicySelector(1)
-        policy = MockPolicy()
+        policy = torch.nn.Module()
         self.store.add_policy('policy1', policy)
         selected_policies = self.store.select_policies(selector)
         self.assertEqual(len(selected_policies), 1)
 
 class TestFilePolicyRecord(unittest.TestCase):
-    @patch('os.rename')
-    @patch('torch.save')
-    def test_save(self, mock_torch_save, mock_os_rename):
-        policy = MagicMock(spec=Policy)
-        record = FilePolicyRecord('test', './test.pt', policy)
+    def test_save(self):
+        policy = torch.nn.Module()
+        record = FilePolicyRecord('test', './test', policy)
         record.save()
-        mock_torch_save.assert_called_once()
-        mock_os_rename.assert_called_once()
 
-    @patch('torch.load')
-    @patch('os.path.exists', return_value=True)
-    def test_load(self, mock_os_path_exists, mock_torch_load):
-        policy = MagicMock(spec=Policy)
-        policy = MagicMock(spec=Policy)
-        create_policy_func = MagicMock(return_value=policy)
-        record = FilePolicyRecord('test', './test.pt')
-        record.load(create_policy_func)
-        mock_torch_load.assert_called_once()
-        self.assertIsNotNone(record.policy(create_policy_func))
+    def test_load(self):
+        policy = torch.nn.Module()
+        record = FilePolicyRecord('test', './test')
+        record.load()
 
     def test_policy_property(self):
-        policy = MagicMock(spec=Policy)
-        record = FilePolicyRecord('test', './test.pt', policy)
+        policy = torch.nn.Module()
+        record = FilePolicyRecord('test', './test', policy)
         self.assertEqual(record.policy(), policy)
 
 class TestDirectoryPolicyStore(unittest.TestCase):
     @patch('os.listdir', return_value=['test1.pt', 'test2.pt'])
     def test_all_policies(self, mock_listdir):
-        create_policy_func = MagicMock()
-        store = DirectoryPolicyStore('./', create_policy_func)
+        store = DirectoryPolicyStore('./')
         policies = store._all_policies()
         self.assertEqual(len(policies), 2)
         self.assertIsInstance(policies['test1'], FilePolicyRecord)
@@ -116,9 +102,8 @@ class TestDirectoryPolicyStore(unittest.TestCase):
 
     @patch.object(FilePolicyRecord, 'save')
     def test_add_policy(self, mock_save):
-        policy = MagicMock(spec=Policy)
-        create_policy_func = MagicMock()
-        store = DirectoryPolicyStore('./', create_policy_func)
+        policy = torch.nn.Module()
+        store = DirectoryPolicyStore('./')
         record = store.add_policy('test', policy)
         mock_save.assert_called_once()
         self.assertEqual(record.name, 'test')
