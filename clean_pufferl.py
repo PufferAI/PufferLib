Fix single-agent envs with masking

clean_pufferl.py
@@ -260,7 +260,7 @@ def evaluate(data):
         padded_steps_collected += len(mask)
         with inference_profiler, torch.no_grad():
             actions, logprob, value, data.next_lstm_state[buf] = data.policy_god.forwards(
-                    o.to(data.device), data.next_lstm_state[buf], None) #TODO: Remove dones from this api
+                    o.to(data.device), data.next_lstm_state[buf])
             value = value.flatten()
 
         # Index alive mask with policy pool idxs...

pufferlib/frameworks/cleanrl.py
@@ -35,11 +35,11 @@ class Policy(torch.nn.Module):
         super().__init__()
         self.policy = policy
 
-    def get_value(self, x, state=None, done=None):
+    def get_value(self, x, state=None):
         _, value = self.policy(x)
         return value
 
-    def get_action_and_value(self, x, action=None, done=None):
+    def get_action_and_value(self, x, action=None):
          logits, value = self.policy(x)
          action, logprob, entropy = sample_logits(logits, action)
          return action, logprob, entropy, value
@@ -60,10 +60,10 @@ class RecurrentPolicy(torch.nn.Module):
         else:
             raise ValueError('Policy must have a subnetwork named lstm or recurrent')
 
-    def get_value(self, x, state=None, done=None):
-        _, value, _ = self.policy(x, state, done)
+    def get_value(self, x, state=None):
+        _, value, _ = self.policy(x, state)
 
-    def get_action_and_value(self, x, state=None, action=None, done=None):
-        logits, value, state = self.policy(x, state, done)
+    def get_action_and_value(self, x, state=None, action=None):
+        logits, value, state = self.policy(x, state)
         action, logprob, entropy = sample_logits(logits, action)
         return action, logprob, entropy, value, state

pufferlib/models.py
@@ -89,7 +89,6 @@ class Policy(nn.Module):
         actions, value = self.decode_actions(hidden, lookup)
         return actions, value
 
-
 class RecurrentWrapper(Policy):
     def __init__(self, env, policy, input_size=128, hidden_size=128, num_layers=1):
         super().__init__(env)
@@ -110,7 +109,7 @@ class RecurrentWrapper(Policy):
             elif "weight" in name:
                 nn.init.orthogonal_(param, 1.0)
 
-    def forward(self, x, state, done):
+    def forward(self, x, state):
         shape, tensor_shape = x.shape, self.observation_space.shape
         x_dims, tensor_dims = len(shape), len(tensor_shape)
         if x_dims == tensor_dims + 1:
@@ -129,22 +128,6 @@ class RecurrentWrapper(Policy):
 
         hidden = hidden.reshape(B, TT, self.input_size)
         hidden = hidden.transpose(0, 1)
-
-        # This block is the technically correct way to handle resets
-        # It also kills ~70% of the performance and is usually not worth it
-        '''
-        done = done.reshape(B, TT)
-        done = done.transpose(0, 1).unsqueeze(2)
-        new_hidden = []
-        for t in range(TT):
-            h = hidden[t:t+1]
-            d = done[t:t+1]
-            state = (state[0] * (1 - d), state[1] * (1 - d))
-            h, state = self.recurrent(h, state)
-            new_hidden.append(h)
-        hidden = torch.cat(new_hidden, dim=0)
-        '''
-
         hidden, state = self.recurrent(hidden, state)
 
         hidden = hidden.transpose(0, 1)
@@ -152,7 +135,6 @@ class RecurrentWrapper(Policy):
 
         hidden, critic = self.policy.decode_actions(hidden, lookup)
         return hidden, critic, state
-
 
 class Default(Policy):
     def __init__(self, env, input_size=128, hidden_size=128):

pufferlib/vectorization/gym_multi_env.py
@@ -18,7 +18,8 @@ def reset(state, seed=None):
         else:
             ob, i = e.reset(seed=hash(1000*seed + idx))
 
-        infos.append({})
+        i['mask'] = True
+        infos.append(i)
         if state.preallocated_obs is None:
             state.preallocated_obs = np.empty(
                 (len(state.envs), *ob.shape), dtype=ob.dtype)
@@ -40,14 +41,14 @@ def step(state, actions):
             rewards.append(0)
             dones.append(False)
             truncateds.append(False)
-            infos.append(i)
         else:
             o, r, d, t, i = env.step(atns)
             rewards.append(r)
             dones.append(d)
             truncateds.append(t)
-            infos.append(i)
 
+        i['mask'] = True
+        infos.append(i)
         state.preallocated_obs[idx] = o
 
     return state.preallocated_obs, rewards, dones, truncateds, infos