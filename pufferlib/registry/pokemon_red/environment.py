06d5dbaa4961b09e560900cea5ff62f05d36a58a
Joseph Suarez <sealsuarez@gmail.com>
2023-10-21 05:28:03 +0000

    Pokemon red registry

---
+++ pufferlib/registry/pokemon_red/environment.py
@@ -0,0 +1,13 @@
+from pdb import set_trace as T
+import pufferlib.emulation
+
+from pufferlib.environments import PokemonRed as env_creator
+
+
+def make_env():
+    '''Pokemon Red'''
+    env = env_creator()
+    ob = env.reset()[0]
+    # Save ob as rgb image
+    #import cv2; cv2.imwrite('ob.png', ob.astype('uint8'))
+    return pufferlib.emulation.GymnasiumPufferEnv(env=env)
