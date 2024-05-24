9c9a37abcc5739d702b4e328254e57b5995d6bb6
Joseph Suarez <jsuarez@puffer.ai>
2024-05-24 00:37:46 +0000

    fix pokemon test

--- test_pokemon_red.py
+++ test_pokemon_red.py
@@ -1,7 +1,9 @@
-from pufferlib.environments import PokemonRed
+from pufferlib.environments.pokemon_red import env_creator
 
-env = PokemonRed()
+env = env_creator()()
 ob, info = env.reset()
 for i in range(100):
     ob, reward, terminal, truncated, info = env.step(env.action_space.sample())
     print(f'Step: {i}, Info: {info}')
+
+env.close()
