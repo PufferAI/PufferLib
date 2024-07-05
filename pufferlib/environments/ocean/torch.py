a072026757fb8f3f8b984a4c317bf991fc4e4e94
Joseph Suarez <jsuarez@puffer.ai>
2024-07-05 02:16:43 +0000

    Sweep architecture

--- pufferlib/environments/ocean/torch.py
+++ pufferlib/environments/ocean/torch.py
@@ -6,13 +6,10 @@ from functools import partial
 import pufferlib.models
 
 Recurrent = pufferlib.models.LSTMWrapper
-#Policy = pufferlib.models.Default
 
 class Policy(nn.Module):
     #def __init__(self, env, flat_size=144,
-    def __init__(self, env, flat_size=32,
-            input_size=32, hidden_size=128, output_size=128,
-            downsample=1, **kwargs):
+    def __init__(self, env, cnn_channels=32, hidden_size=128, **kwargs):
         super().__init__()
         '''
         self.network= nn.Sequential(
@@ -26,18 +23,20 @@ class Policy(nn.Module):
         )
         '''
         self.network= nn.Sequential(
-            pufferlib.pytorch.layer_init(nn.Conv2d(8, 32, 5, stride=3)),
+            pufferlib.pytorch.layer_init(
+                nn.Conv2d(8, cnn_channels, 5, stride=3)),
             nn.ReLU(),
-            pufferlib.pytorch.layer_init(nn.Conv2d(32, 32, 3, stride=1)),
+            pufferlib.pytorch.layer_init(
+                nn.Conv2d(cnn_channels, cnn_channels, 3, stride=1)),
             nn.ReLU(),
             nn.Flatten(),
-            pufferlib.pytorch.layer_init(nn.Linear(flat_size, hidden_size)),
+            pufferlib.pytorch.layer_init(nn.LazyLinear(hidden_size)),
             nn.ReLU(),
         )
         self.actor = pufferlib.pytorch.layer_init(
             nn.Linear(hidden_size, env.single_action_space.n), std=0.01)
         self.value_fn = pufferlib.pytorch.layer_init(
-            nn.Linear(output_size, 1), std=1)
+            nn.Linear(hidden_size, 1), std=1)
 
     def forward(self, observations):
         hidden, lookup = self.encode_observations(observations)
