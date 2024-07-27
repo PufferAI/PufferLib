bbfc2450433a3b5fdf5d5f09d513d32ba801d2c9
Joseph Suarez <jsuarez@puffer.ai>
2024-07-27 19:06:58 +0000

    Experimental faster atari

--- pufferlib/environments/atari/environment.py
+++ pufferlib/environments/atari/environment.py
@@ -9,67 +9,39 @@ import pufferlib.emulation
 import pufferlib.environments
 import pufferlib.utils
 import pufferlib.postprocess
-
-ALIASES = {
-    'beam-rider': 'BeamRiderNoFrameskip-v4',
-    'breakout': 'BreakoutNoFrameskip-v4',
-    'enduro': 'EnduroNoFrameskip-v4',
-    'pong': 'PongNoFrameskip-v4',
-    'qbert': 'QbertNoFrameskip-v4',
-    'seaquest': 'SeaquestNoFrameskip-v4',
-    'spaceinvaders': 'SpaceInvadersNoFrameskip-v4',
-}
-
+import pufferlib.postprocess
 
 def env_creator(name='breakout'):
     return functools.partial(make, name)
 
-def make(name, framestack=4, render_mode='rgb_array'):
-    '''Atari creation function with default CleanRL preprocessing based on Stable Baselines3 wrappers'''
-    pufferlib.environments.try_import('ale_py', 'atari')
-    from stable_baselines3.common.atari_wrappers import (
-        ClipRewardEnv,
-        EpisodicLifeEnv,
-        FireResetEnv,
-        MaxAndSkipEnv,
-        NoopResetEnv,
-    )
-
-    # Convert aliases to full names
-    if name in ALIASES:
-        name = ALIASES[name]
-
-    with pufferlib.utils.Suppress():
-        env = gym.make(name, render_mode=render_mode)
-
-    env = gym.wrappers.RecordEpisodeStatistics(env)
-    env = NoopResetEnv(env, noop_max=30)
-    env = MaxAndSkipEnv(env, skip=4)
-    env = EpisodicLifeEnv(env)
-    if "FIRE" in env.unwrapped.get_action_meanings():
-        env = FireResetEnv(env)
-    env = ClipRewardEnv(env)
-    env = gym.wrappers.ResizeObservation(env, (84, 84))
-    env = gym.wrappers.GrayScaleObservation(env)
-    env = gym.wrappers.FrameStack(env, framestack)
+def make(name, obs_type='grayscale', frameskip=4, render_mode='rgb_array'):
+    '''Atari creation function'''
+    pufferlib.environments.try_import('ale_py', 'AtariEnv')
+    from ale_py import AtariEnv
+    env = AtariEnv(name, obs_type='grayscale',
+        frameskip=frameskip, render_mode=render_mode)
+    env = pufferlib.postprocess.ResizeObservation(env, downscale=2)
     env = AtariPostprocessor(env) # Don't use standard postprocessor
-    return pufferlib.emulation.GymnasiumPufferEnv(env=env)
+    env = pufferlib.emulation.GymnasiumPufferEnv(env=env)
+    return env
 
 class AtariPostprocessor(gym.Wrapper):
     '''Atari breaks the normal PufferLib postprocessor because
     it sends terminal=True every live, not every episode'''
     def __init__(self, env):
         super().__init__(env)
+        self.observation_space = gym.spaces.Box(
+            low=0, high=255, shape=(1, *env.observation_space.shape),
+            dtype=env.observation_space.dtype)
 
     def reset(self, seed=None):
         obs, info = self.env.reset(seed=seed)
-        return obs, {}
+        return np.expand_dims(obs, 0), {}
 
     def step(self, action):
         obs, reward, terminal, truncated, info = self.env.step(action)
         if 'episode' not in info:
             info = {}
 
-        return obs, reward, terminal, truncated, info
-
+        return np.expand_dims(obs, 0), reward, terminal, truncated, info
 
