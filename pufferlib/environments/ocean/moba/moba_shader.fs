01699c470b6efb244b8aa86bf799e6dbb5ae8915
Joseph Suarez <jsuarez@puffer.ai>
2024-08-22 23:15:30 +0000

    prettify

--- pufferlib/environments/ocean/moba/moba_shader.fs
+++ pufferlib/environments/ocean/moba/moba_shader.fs
@@ -47,7 +47,7 @@ void main()
     //vec2 scaledTexCoord = vec2(fragTexCoord.x/SCALE_FACTOR + camera_x, fragTexCoord.y/SCALE_FACTOR + camera_y);
     //float dist = DIST / textureSize(texture1, 0).x;
 
-	float borderColor = 0.75 + 0.25*sin(2*time);
+	float borderColor = 0.875 + 0.125*sin(2*time);
 
     // Texel color fetching from texture sampler with scaled coordinates
     vec4 texelColor = texture(texture1, scaledTexCoord) * colDiffuse * fragColor;
@@ -75,19 +75,55 @@ void main()
     	(texelColor != texelLeftDown) ||
     	(texelColor != texelRightUp) ||
     	(texelColor != texelLeftUp));
-    
+
+    float lerp = 10*(scaledTexCoord.x - scaledTexCoord.y);
+    float lerp_red = clamp(lerp, 0, 1);
+    float lerp_cyan = clamp(1.0-lerp, 0, 1);
+
     if (isBorder) {
         // Change border pixels to (0, 128, 128, 255)
-        outputColor = vec4(0.0, borderColor, borderColor, 1.0);
+        outputColor = vec4(lerp_red*borderColor, lerp_cyan*borderColor, (lerp_cyan+0.5)*borderColor, 1.0);
     } else if (texelColor.rgb == vec3(1.0, 1.0, 1.0)) {
         // Change white pixels to (6, 24, 24, 255)
-        outputColor = vec4(6.0/255.0, 24.0/255.0, 24.0/255.0, 1.0);
+        outputColor = vec4(18.0/255.0*lerp_red + 6.0/255.0, 18.0/255.0*lerp_cyan + 6.0/255.0, 18.0/255.0*lerp_cyan + 6.0/255.0, 1.0);
     } else if (texelColor.rgb == vec3(0.0, 0.0, 0.0)) {
         // Change black pixels to cyan (0, 255, 255, 255)
-        outputColor = vec4(0.0, 0.5, 0.5, 1.0);
+        outputColor = vec4(0.5*lerp_red, 0.5*lerp_cyan, 0.5*lerp_cyan, 1.0);
     } else {
         // Keep other colors unchanged
         outputColor = texelColor;
     }
+ 
+    /*
+    if (scaledTexCoord.x < scaledTexCoord.y) {
+        if (isBorder) {
+            // Change border pixels to (0, 128, 128, 255)
+            outputColor = vec4(0.0, borderColor, borderColor, 1.0);
+        } else if (texelColor.rgb == vec3(1.0, 1.0, 1.0)) {
+            // Change white pixels to (6, 24, 24, 255)
+            outputColor = vec4(6.0/255.0, 24.0/255.0, 24.0/255.0, 1.0);
+        } else if (texelColor.rgb == vec3(0.0, 0.0, 0.0)) {
+            // Change black pixels to cyan (0, 255, 255, 255)
+            outputColor = vec4(0.0, 0.5, 0.5, 1.0);
+        } else {
+            // Keep other colors unchanged
+            outputColor = texelColor;
+        }
+    } else {
+        if (isBorder) {
+            // Change border pixels to (128, 0, 0, 255)
+            outputColor = vec4(borderColor, 0.0, 0.0, 1.0);
+        } else if (texelColor.rgb == vec3(1.0, 1.0, 1.0)) {
+            // Change white pixels to (24, 6, 6, 255)
+            outputColor = vec4(24.0/255.0, 6.0/255.0, 6.0/255.0, 1.0);
+        } else if (texelColor.rgb == vec3(0.0, 0.0, 0.0)) {
+            // Change black pixels to red (255, 0, 0, 255)
+            outputColor = vec4(0.5, 0.0, 0.0, 1.0);
+        } else {
+            // Keep other colors unchanged
+            outputColor = texelColor;
+        }
+    }
+    */
 }
 
