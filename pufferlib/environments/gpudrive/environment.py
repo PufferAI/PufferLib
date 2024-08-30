e43057dbf6b0e7e2433d86274e6ced27a5df58b0
Joseph Suarez <jsuarez@puffer.ai>
2024-08-30 23:05:54 +0000

    exorcise GPUDrive wrapper bugs

--- pufferlib/environments/gpudrive/environment.py
+++ pufferlib/environments/gpudrive/environment.py
@@ -8,10 +8,10 @@ from pygpudrive.env.config import EnvConfig, RenderConfig, SceneConfig, Selectio
 from pygpudrive.env.env_torch import GPUDriveTorchEnv
 
 def env_creator(name='gpudrive'):
-    return PufferCPUDrive
+    return PufferGPUDrive
 
-class PufferCPUDrive:
-    def __init__(self, device='cpu', max_cont_agents=64, num_worlds=64, k_unique_scenes=1):
+class PufferGPUDrive:
+    def __init__(self, device='cuda', max_cont_agents=64, num_worlds=64, k_unique_scenes=1):
         self.device = device
         self.max_cont_agents = max_cont_agents
         self.num_worlds = num_worlds
@@ -65,9 +65,6 @@ class PufferCPUDrive:
         self.obs = self.env.reset()[self.controlled_agent_mask]
         self.num_controlled = self.controlled_agent_mask.sum().item()
         self.num_agents = self.obs.shape[0]
-        self.reward = torch.zeros(self.num_agents, dtype=torch.float32).to(self.device)
-        self.terminal = torch.zeros(self.num_agents, dtype=torch.bool).to(self.device)
-        self.truncated = torch.zeros(self.num_agents, dtype=torch.bool).to(self.device)
         self.env_id = np.array([i for i in range(self.num_agents)])
         self.mask = np.ones(self.num_agents, dtype=bool)
         self.actions = torch.zeros((num_worlds, max_cont_agents),
@@ -88,7 +85,12 @@ class PufferCPUDrive:
         #del self.env.sim
 
     def reset(self, seed=None, options=None):
+        self.reward = torch.zeros(self.num_agents, dtype=torch.float32).to(self.device)
+        self.terminal = torch.zeros(self.num_agents, dtype=torch.bool).to(self.device)
+        self.truncated = torch.zeros(self.num_agents, dtype=torch.bool).to(self.device)
+
         self.episode_returns = torch.zeros(self.num_agents, dtype=torch.float32).to(self.device)
+        self.episode_lengths = torch.zeros(self.num_agents, dtype=torch.float32).to(self.device)
         self.live_agent_mask = torch.ones((self.num_worlds, self.max_cont_agents), dtype=bool).to(self.device)
         return self.obs, self.reward, self.terminal, self.truncated, [], self.env_id, self.mask
 
@@ -107,22 +109,25 @@ class PufferCPUDrive:
         )[0].cpu()
 
         self.episode_returns += reward
+        self.episode_lengths += 1
+        self.mask = self.live_agent_mask[self.controlled_agent_mask].cpu().numpy()
         self.live_agent_mask[terminal] = 0
         terminal = terminal[self.controlled_agent_mask]
-        self.mask = self.live_agent_mask[self.controlled_agent_mask].cpu().numpy()
 
         info = []
         self.num_live.append(self.mask.sum())
 
         if len(done_worlds) > 0:
-            info_tensor = self.env.get_infos()[done_worlds]
-            num_finished_agents = self.controlled_agent_mask[done_worlds].sum().item()
+            controlled_mask = self.controlled_agent_mask[done_worlds]
+            info_tensor = self.env.get_infos()[done_worlds][controlled_mask]
+            num_finished_agents = controlled_mask.sum().item()
             info.append({
-                'off_road': info_tensor[:, :, 0].sum().item() / num_finished_agents,
-                'veh_collisions': info_tensor[:, :, 1].sum().item() / num_finished_agents,
-                'non_veh_collisions': info_tensor[:, :, 2].sum().item() / num_finished_agents,
-                'goal_achieved': info_tensor[:, :, 3].sum().item() / num_finished_agents,
+                'off_road': info_tensor[:, 0].sum().item() / num_finished_agents,
+                'veh_collisions': info_tensor[:, 1].sum().item() / num_finished_agents,
+                'non_veh_collisions': info_tensor[:, 2].sum().item() / num_finished_agents,
+                'goal_achieved': info_tensor[:, 3].sum().item() / num_finished_agents,
                 'num_finished_agents': num_finished_agents,
+                'episode_length': self.episode_lengths[done_worlds].mean().item(),
                 'mean_reward_per_episode': self.episode_returns[done_worlds].mean().item(),
                 'control_density': self.num_controlled / self.num_agents,
                 'data_density': np.mean(self.num_live) / self.num_agents,
@@ -132,6 +137,7 @@ class PufferCPUDrive:
             for idx in done_worlds:
                 self.env.sim.reset(idx)
                 self.episode_returns[idx] = 0
+                self.episode_lengths[idx] = 0
                 self.live_agent_mask[idx] = self.controlled_agent_mask[idx]
 
         return obs, reward, terminal, self.truncated, info, self.env_id, self.mask
