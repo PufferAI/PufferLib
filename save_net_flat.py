f16fa25ff12368eb3d23f2568af7432a03cf6185
Joseph Suarez <jsuarez@puffer.ai>
2024-09-06 01:06:22 +0000

    flat net

--- save_net_flat.py
+++ save_net_flat.py
@@ -1,4 +1,5 @@
 import torch
+from torch.nn import functional as F
 import numpy as np
 
 def save_model_weights(model, filename):
@@ -16,7 +17,30 @@ def save_model_weights(model, filename):
 
 def test_model(model):
     model = model.cpu().policy
-    x = torch.arange(0, 121*16).view(16, 121).float() / 1000
+    batch_size = 16
+    obs_window = 11
+    obs_window_channels = 4
+    obs_flat = 26
+    x = torch.arange(
+        0, batch_size*(obs_window*obs_window*obs_window_channels + obs_flat)
+        ).reshape(batch_size, -1) % 16
+
+    cnn_features = x[:, :-obs_flat].view(
+        batch_size, obs_window, obs_window, obs_window_channels).long()
+    map_features = F.one_hot(cnn_features[:, :, :, 0], 16).permute(0, 3, 1, 2).float()
+    extra_map_features = (cnn_features[:, :, :, -3:].float() / 255.0).permute(0, 3, 1, 2)
+    cnn_features = torch.cat([map_features, extra_map_features], dim=1)
+    cnn = model.policy.cnn
+
+    cnn_features = torch.from_numpy(
+        np.arange(batch_size*11*11*19).reshape(
+        batch_size, 19, obs_window, obs_window)
+    ).float()
+    conv1_out = cnn[0](cnn_features)
+
+    #(cnn[0].weight[0] * cnn_features[0, :, :5, :5]).sum() + cnn[0].bias[0]
+
+    breakpoint()
     hidden = model.encoder(x)
     output = model.decoder(hidden)
     atn = output.argmax(dim=1)
@@ -32,6 +56,7 @@ def test_model(model):
 
 	
 if __name__ == '__main__':
-    model = torch.load('snake_mlp.pt')
-    #test_model(model)
-    save_model_weights(model, 'flat_snake.pt')
+    model = torch.load('moba.pt')
+    test_model(model)
+
+    #save_model_weights(model, 'moba_weights.pt')
