44aa200b8c56a23acbfa897205ae27da01779b06
Joseph Suarez <jsuarez@puffer.ai>
2024-09-09 21:44:09 +0000

    More fixes

--- pufferlib/environments/ocean/moba/moba.c
+++ pufferlib/environments/ocean/moba/moba.c
@@ -1,140 +1,113 @@
 #include "moba.h"
+#include "puffernet.h"
 
-// File format is obained by flattening and concatenating all pytorch layers
-int load_weights(const char* filename, float* weights, int num_weights) {
-    FILE* file = fopen(filename, "rb");
-    if (!file) {
-        perror("Error opening file");
-        return 1;
-    }
-    fseek(file, 0, SEEK_END);
-    rewind(file);
-    size_t read_size = fread(weights, sizeof(float), num_weights, file);
-    fclose(file);
-    if (read_size != num_weights) {
-        perror("Error reading file");
-        return 1;
-    }
-    return 0;
+typedef struct MOBANet MOBANet;
+struct MOBANet {
+    int num_agents;
+    float* obs_2d;
+    float* obs_1d;
+    Conv2D* conv1;
+    ReLU* relu1;
+    Conv2D* conv2;
+    Linear* flat;
+    CatDim1* cat;
+    ReLU* relu2;
+    Linear* proj;
+    ReLU* relu3;
+    LSTM* lstm;
+    Linear* actor;
+    Linear* value_fn;
+    ArgmaxMultidiscrete* multidiscrete;
+};
+
+MOBANet* init_mobanet(Weights* weights, int num_agents) {
+    MOBANet* net = calloc(1, sizeof(MOBANet));
+    net->num_agents = num_agents;
+    net->obs_2d = calloc(num_agents*11*11*19, sizeof(float));
+    net->obs_1d = calloc(num_agents*26, sizeof(float));
+    printf("First weight: %f\n", weights->data[0]);
+    printf("Weight index: %d\n", weights->idx);
+    net->conv1 = make_conv2d(weights, num_agents, 11, 11, 19, 32, 5, 3);
+    net->relu1 = make_relu(num_agents, 32*3*3);
+    net->conv2 = make_conv2d(weights, num_agents, 3, 3, 32, 32, 3, 1);
+    net->flat = make_linear(weights, num_agents, 26, 32);
+    net->cat = make_cat_dim1(num_agents, 32, 32);
+    net->relu2 = make_relu(num_agents, 64);
+    net->proj = make_linear(weights, num_agents, 64, 128);
+    net->relu3 = make_relu(num_agents, 128);
+    net->actor = make_linear(weights, num_agents, 128, 23);
+    net->value_fn = make_linear(weights, num_agents, 128, 1);
+    net->lstm = make_lstm(weights, num_agents, 128, 128);
+    int logit_sizes[6] = {7, 7, 3, 2, 2, 2};
+    net->multidiscrete = make_argmax_multidiscrete(num_agents, logit_sizes, 6);
+
+    printf("conv1 weight 0: %f\n", net->conv1->weights[0]);
+    printf("conv1 bias 0: %f\n", net->conv1->bias[0]);
+    printf("conv2 weight 0: %f\n", net->conv2->weights[0]);
+    printf("conv2 bias 0: %f\n", net->conv2->bias[0]);
+    printf("flat weight 0: %f\n", net->flat->weights[0]);
+    printf("flat bias 0: %f\n", net->flat->bias[0]);
+    printf("proj weight 0: %f\n", net->proj->weights[0]);
+    printf("proj bias 0: %f\n", net->proj->bias[0]);
+    printf("actor weight 0: %f\n", net->actor->weights[0]);
+    printf("actor bias 0: %f\n", net->actor->bias[0]);
+    printf("value_fn weight 0: %f\n", net->value_fn->weights[0]);
+    printf("value_fn bias 0: %f\n", net->value_fn->bias[0]);
+    printf("lstm weight 0: %f\n", net->lstm->weights_input[0]);
+    printf("lstm bias 0: %f\n", net->lstm->bias_input[0]);
+    printf("lstm weight 1: %f\n", net->lstm->weights_state[0]);
+    printf("lstm bias 1: %f\n", net->lstm->bias_state[0]);
+    return net;
 }
 
-void linear_layer(float* input, float* weights, float* bias, float* output,
-        int batch_size, int input_dim, int output_dim) {
-    for (int b = 0; b < batch_size; b++) {
-        for (int o = 0; o < output_dim; o++) {
-            float sum = 0.0f;
-            for (int i = 0; i < input_dim; i++)
-                sum += input[b*input_dim + i] * weights[o*input_dim + i];
-            output[b*output_dim + o] = sum + bias[o];
-        }
-    }
+void free_mobanet(MOBANet* net) {
+    free_conv2d(net->conv1);
+    free_relu(net->relu1);
+    free_conv2d(net->conv2);
+    free_linear(net->flat);
+    free_linear(net->proj);
+    free_linear(net->actor);
+    free_linear(net->value_fn);
 }
 
-void linear_layer_accumulate(float* input, float* weights, float* bias, float* output,
-        int batch_size, int input_dim, int output_dim) {
-    for (int b = 0; b < batch_size; b++) {
-        for (int o = 0; o < output_dim; o++) {
-            float sum = 0.0f;
-            for (int i = 0; i < input_dim; i++)
-                sum += input[b*input_dim + i] * weights[o*input_dim + i];
-            output[b*output_dim + o] += sum + bias[o];
-        }
-    }
-}
-
-void relu(float* data, int size) {
-    for (int i = 0; i < size; i++)
-        data[i] = fmaxf(0.0f, data[i]);
-}
-
-float sigmoid(float x);
-inline float sigmoid(float x) {
-    return 1.0f / (1.0f + expf(-x));
-}
-
-void convolution_layer(float* input, float* weights, float* bias,
-        float* output, int batch_size, int in_width, int in_height,
-        int in_channels, int out_channels, int kernel_size, int stride) {
-    int h_out = (in_height - kernel_size)/stride + 1;
-    int w_out = (in_width - kernel_size)/stride + 1;
-    for (int b = 0; b < batch_size; b++) {
-        for (int oc = 0; oc < out_channels; oc++) {
-            for (int h = 0; h < h_out; h++) {
-                for (int w = 0; w < w_out; w++) {
-                    int out_adr = (
-                        b*out_channels*h_out*w_out
-                        + oc*h_out*w_out+ 
-                        + h*w_out
-                        + w
-                    );
-                    output[out_adr] = bias[oc];
-                    for (int ic = 0; ic < in_channels; ic++) {
-                        for (int kh = 0; kh < kernel_size; kh++) {
-                            for (int kw = 0; kw < kernel_size; kw++) {
-                                int in_adr = (
-                                    b*in_channels*in_height*in_width
-                                    + ic*in_height*in_width
-                                    + (h*stride + kh)*in_width
-                                    + (w*stride + kw)
-                                );
-                                int weight_adr = (
-                                    oc*in_channels*kernel_size*kernel_size
-                                    + ic*kernel_size*kernel_size
-                                    + kh*kernel_size
-                                    + kw
-                                );
-                                output[out_adr] += input[in_adr]*weights[weight_adr];
-                            }
-                        }
-                    }
-               }
+void forward(MOBANet* net, unsigned char* env_obs_2d, unsigned char* env_obs_1d, int* actions) {
+    memset(net->obs_2d, 0, net->num_agents*11*11*19*sizeof(float));
+    float (*obs_2d)[19][11][11] = (float (*)[19][11][11])net->obs_2d;
+    float (*obs_1d)[26] = (float (*)[26])net->obs_1d;
+    for (int b = 0; b < net->num_agents; b++) {
+        int b_offset = b*(11*11*4);
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 11; j++) {
+                int elem_offset = 4*(i*11 + j);
+                int one_hot_idx = env_obs_2d[b_offset + elem_offset];
+                obs_2d[b][one_hot_idx][i][j] = 1;
+                obs_2d[b][16][i][j] = env_obs_2d[b_offset + 2*elem_offset] / 255.0f;
+                obs_2d[b][17][i][j] = env_obs_2d[b_offset + 3*elem_offset] / 255.0f;
+                obs_2d[b][18][i][j] = env_obs_2d[b_offset + 4*elem_offset] / 255.0f;
             }
         }
-    }
-}
-
-void lstm(float* input, float* state_h, float* state_c, float* weights_input,
-        float* weights_state, float* bias_input, float*bias_state,
-        float *buffer, int batch_size, int input_size, int hidden_size) {
-    linear_layer(input, weights_input, bias_input, buffer, batch_size, input_size, 4*hidden_size);
-    linear_layer_accumulate(state_h, weights_state, bias_state, buffer, batch_size, hidden_size, 4*hidden_size);
-
-    // Activation functions
-    for (int b=0; b<batch_size; b++) {
-        int b_offset = 4*b*hidden_size;
-        for (int i=0; i<2*hidden_size; i++) {
-            int buf_adr = b_offset + i;
-            buffer[buf_adr] = sigmoid(buffer[buf_adr]);
-        }
-        for (int i=2*hidden_size; i<3*hidden_size; i++) {
-            int buf_adr = b_offset + i;
-            buffer[buf_adr] = tanh(buffer[buf_adr]);
-        }
-        for (int i=3*hidden_size; i<4*hidden_size; i++) {
-            int buf_adr = b_offset + i;
-            buffer[buf_adr] = sigmoid(buffer[buf_adr]);
+        for (int i = 0; i < 26; i++) {
+            obs_1d[b][i] = env_obs_1d[b*26 + i] / 255.0f;
         }
     }
 
-    // Gates
-    for (int b=0; b<batch_size; b++) {
-        int inp_offset = b*hidden_size;
-        int b_offset = 4*b*hidden_size;
-        for (int i=0; i<hidden_size; i++) {
-            state_c[inp_offset + i] = (
-                buffer[b_offset + hidden_size + i] * input[inp_offset + i]
-                + buffer[b_offset + i] * buffer[b_offset + 2*hidden_size + i]
-            );
-            state_h[inp_offset + i] = (
-                buffer[b_offset + 3*hidden_size + i] * tanh(state_c[inp_offset + i])
-            );
-        }
-    }
- 
-}
+    conv2d(net->conv1, net->obs_2d);
+    relu(net->relu1, net->conv1->output);
+    conv2d(net->conv2, net->relu1->output);
 
-float get_out(float* output, int b, int oc, int h, int w) {
-    return output[(b*32*3*3) + (oc*3*3) + (h*3) + w];
+    linear(net->flat, net->obs_1d);
+
+    cat_dim1(net->cat, net->conv2->output, net->flat->output);
+    relu(net->relu2, net->cat->output);
+    linear(net->proj, net->relu2->output);
+    relu(net->relu3, net->proj->output);
+    
+    lstm(net->lstm, net->proj->output);
+
+    linear(net->actor, net->lstm->output);
+    linear(net->value_fn, net->lstm->output);
+
+    argmax_multidiscrete(net->multidiscrete, net->actor->output, actions);
 }
 
 int main() {
@@ -150,66 +123,8 @@ int main() {
     float reward_distance = 0.05;
     float reward_tower = 3.0;
 
-    /*
-    int num_weights = 32*19*5*5;
-    int num_bias = 32;
-    float* weights = calloc(num_weights + num_bias, sizeof(float));
-    float* bias = &weights[num_weights];
-    load_weights("moba_weights.pt", weights, num_weights+num_bias) ;
-
-    int batch_size = 16;
-    int num_input = batch_size*(11*11*19 + 26);
-    float *input = calloc(num_input, sizeof(float));
-    for (int i = 0; i < num_input; i++) {
-        input[i] = i;
-    }
-
-    int num_output = batch_size*32*3*3;
-    float* output = calloc(num_output, sizeof(float));
-
-    convolution_layer(input, weights, bias, output, batch_size, 11, 11, 19, 32, 5, 3);
-    */
-
-    int batch_size = 16;
-    int input_size = 128;
-    int hidden_size = 128;
-
-    int num_input = batch_size*input_size;
-    int num_buffer = 4*batch_size*hidden_size;
-    int num_output = batch_size*hidden_size;
-
-    float* input = calloc(num_input, sizeof(float));
-    float* buffer = calloc(num_buffer, sizeof(float));
-    float* state_h = calloc(num_output, sizeof(float));
-    float* state_c = calloc(num_output, sizeof(float));
-    float* weights_input = calloc(4*hidden_size*input_size, sizeof(float));
-    float* weights_state = calloc(4*hidden_size*hidden_size, sizeof(float));
-    float* bias_input = calloc(4*hidden_size, sizeof(float));
-    float* bias_state = calloc(4*hidden_size, sizeof(float));
-
-    for (int i = 0; i < num_input; i++) {
-        input[i] = i / 100000.0f;
-    }
-    for (int i = 0; i < num_buffer; i++) {
-        buffer[i] = i / 100000.0f;
-    }
-    for (int i = 0; i < num_output; i++) {
-        state_h[i] = i / 100000.0f;
-        state_c[i] = i / 100000.0f;
-    }
-    for (int i = 0; i < 4*hidden_size*input_size; i++) {
-        weights_input[i] = i / 100000.0f;
-    }
-    for (int i = 0; i < 4*hidden_size*hidden_size; i++) {
-        weights_state[i] = i / 100000.0f;
-    }
-    for (int i = 0; i < 4*hidden_size; i++) {
-        bias_input[i] = i / 100000.0f;
-        bias_state[i] = i / 100000.0f;
-    }
-
-    lstm(input, state_h, state_c, weights_input, weights_state, bias_input,
-        bias_state, buffer, batch_size, input_size, hidden_size);
+    Weights* weights = load_weights("moba_weights.bin", 168856);
+    MOBANet* net = init_mobanet(weights, num_agents);
 
     MOBA* env = init_moba(num_agents, num_creeps, num_neutrals, num_towers, vision_range,
         agent_speed, discretize, reward_death, reward_xp, reward_distance, reward_tower);
@@ -221,10 +136,12 @@ int main() {
     while (!WindowShouldClose()) {
         if (frame % 12 == 0) {
             step(env);
+            forward(net, env->observations_map, env->observations_extra, env->actions);
         }
         render_game(renderer, env, frame);
         frame = (frame + 1) % 12;
     }
+    free_mobanet(net);
     free_moba(env);
     close_game_renderer(renderer);
     return 0;
