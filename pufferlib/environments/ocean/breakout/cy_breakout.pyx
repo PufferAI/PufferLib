031b62c8781658962690f5cab9a18b2e09949a0a
Joseph Suarez <jsuarez@puffer.ai>
2024-09-23 23:35:25 +0000

    Breakout refactor

--- pufferlib/environments/ocean/breakout/cy_breakout.pyx
+++ pufferlib/environments/ocean/breakout/cy_breakout.pyx
@@ -37,14 +37,10 @@ cdef extern from "breakout.h":
 
     ctypedef struct Client
 
-    CBreakout* init_cbreakout(int frameskip, unsigned char* actions,
-            float* observations, float* rewards, unsigned char* dones,
-            int width, int height, float paddle_width, float paddle_height,
-            int ball_width, int ball_height, int brick_width, int brick_height,
-            int brick_rows, int brick_cols)
-    void free_cbreakout(CBreakout* env)
+    void init(CBreakout* env)
+    void free_initialized(CBreakout* env)
 
-    Client* make_client(float width, float height)
+    Client* make_client(CBreakout* env)
     void close_client(Client* client)
     void render(Client* client, CBreakout* env)
     void reset(CBreakout* env)
@@ -52,7 +48,7 @@ cdef extern from "breakout.h":
 
 cdef class CyBreakout:
     cdef:
-        CBreakout* env
+        CBreakout env
         Client* client
 
     def __init__(self, int frameskip, cnp.ndarray actions,
@@ -60,27 +56,41 @@ cdef class CyBreakout:
             int width, int height, float paddle_width, float paddle_height,
             int ball_width, int ball_height, int brick_width, int brick_height,
             int brick_rows, int brick_cols):
-        self.env = init_cbreakout(frameskip, <unsigned char*> actions.data,
-            <float*> observations.data, <float*> rewards.data, <unsigned char*> dones.data,
-            width, height, paddle_width, paddle_height, ball_width, ball_height,
-            brick_width, brick_height, brick_rows, brick_cols)
+        self.env = CBreakout(
+            observations=<float*> observations.data,
+            actions=<unsigned char*> actions.data,
+            rewards=<float*> rewards.data,
+            dones=<unsigned char*> dones.data,
+            width=width,
+            height=height,
+            paddle_width=paddle_width,
+            paddle_height=paddle_height,
+            ball_width=ball_width,
+            ball_height=ball_height,
+            brick_width=brick_width,
+            brick_height=brick_height,
+            brick_rows=brick_rows,
+            brick_cols=brick_cols,
+            frameskip=frameskip,
+        )
+        init(&self.env)
         self.client = NULL
 
     def reset(self):
-        reset(self.env)
+        reset(&self.env)
 
     def step(self):
-        step(self.env)
+        step(&self.env)
 
     def render(self):
         if self.client == NULL:
-            self.client = make_client(self.env.width, self.env.height)
+            self.client = make_client(&self.env)
 
-        render(self.client, self.env)
+        render(self.client, &self.env)
 
     def close(self):
         if self.client != NULL:
             close_client(self.client)
             self.client = NULL
 
-        free_cbreakout(self.env)
+        free_initialized(&self.env)
