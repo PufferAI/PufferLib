69cd154917d055a424db245ce23566a3766aac0a
Spencer Cheng <spenccheng@gmail.com>
2024-09-24 01:08:42 +0000

    okay think this is good now

---
+++ pufferlib/environments/ocean/connect4/build_local.sh
@@ -0,0 +1,1 @@
+clang -Wall -Wuninitialized -Wmisleading-indentation -fsanitize=address -ferror-limit=3 -g -o connect4game connect4.c -I./raylib-5.0_linux_amd64/include/ -L./raylib-5.0_linux_amd64/lib/ -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -DPLATFORM_DESKTOP  
