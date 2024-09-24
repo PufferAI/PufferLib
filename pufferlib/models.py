3b76c3d4eea48732bd825eebd2b80dee3af16db2
Joseph Suarez <jsuarez@puffer.ai>
2024-09-24 20:42:03 +0000

    Damnit leanke

--- pufferlib/models.py
+++ pufferlib/models.py
@@ -23,7 +23,6 @@ class Default(nn.Module):
     '''
     def __init__(self, env, hidden_size=128):
         super().__init__()
-        self.dtype = pufferlib.pytorch.nativize_dtype(env.emulated)
         self.is_multidiscrete = isinstance(env.single_action_space,
                 pufferlib.spaces.MultiDiscrete)
         self.is_continuous = isinstance(env.single_action_space,
@@ -34,6 +33,7 @@ class Default(nn.Module):
             self.is_dict_obs = isinstance(env.observation_space, pufferlib.spaces.Dict) 
 
         if self.is_dict_obs:
+            self.dtype = pufferlib.pytorch.nativize_dtype(env.emulated)
             input_size = sum(np.prod(v.shape) for v in env.env.observation_space.values())
             self.encoder = nn.Linear(input_size, self.hidden_size)
         else:
