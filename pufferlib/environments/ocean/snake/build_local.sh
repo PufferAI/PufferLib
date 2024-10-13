01342ab321ec1fdf4d94d1d9dd2d91b49581d520
Joseph Suarez <jsuarez@puffer.ai>
2024-10-13 01:19:54 +0000

    snake fixes

--- pufferlib/environments/ocean/snake/build_local.sh
+++
@@ -1,2 +0,0 @@
-#gcc -g -o snakegame snake.c -I./raylib-5.0_linux_amd64/include/ -L./raylib-5.0_linux_amd64/lib/ -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
-clang -fsanitize=address -g -o snakegame snake.c -I./raylib-5.0_linux_amd64/include/ -L./raylib-5.0_linux_amd64/lib/ -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
