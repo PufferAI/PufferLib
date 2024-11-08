3a9218c90bb2967bbfe6c70cb332f854737255d8
Kyoung Whan Choe <choe.kyoung@gmail.com>
2024-11-07 21:16:05 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -280,7 +280,7 @@ setup(
         f'gymnasium<={GYMNASIUM_VERSION}',
         f'pettingzoo<={PETTINGZOO_VERSION}',
         'shimmy[gym-v21]',
-        'psutil==5.9.5',
+        'psutil',
         'pynvml',
         'imageio',
     ],
