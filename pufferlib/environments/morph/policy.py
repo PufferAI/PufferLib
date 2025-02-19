b26ee1d227593878e53394b2d864abe3bb81ff0a
kywch <choe.kyoung@gmail.com>
2025-02-19 11:00:51 -0800

    tweak critic

--- pufferlib/environments/morph/policy.py
+++ pufferlib/environments/morph/policy.py
@@ -63,20 +63,18 @@ class Policy(nn.Module):
         ### Separate Critic
         self.critic_mlp = nn.Sequential(
             layer_init(nn.Linear(input_size, hidden_size)),
+            nn.LayerNorm(hidden_size),
             nn.SiLU(),
             layer_init(nn.Linear(hidden_size, hidden_size)),
+            nn.LayerNorm(hidden_size),
             nn.SiLU(),
             layer_init(nn.Linear(hidden_size, 512)),
-            nn.SiLU(),
-            layer_init(nn.Linear(512, 512)),
+            nn.LayerNorm(512),
             nn.SiLU(),
             layer_init(nn.Linear(512, 256)),
+            nn.LayerNorm(256),
             nn.SiLU(),
-            layer_init(nn.Linear(256, 256)),
-            nn.SiLU(),
-            layer_init(nn.Linear(256, 128)),
-            nn.SiLU(),
-            layer_init(nn.Linear(128, 1)),
+            layer_init(nn.Linear(256, 1), std=0.01),
         )
 
         # NOTE: Original PHC network
