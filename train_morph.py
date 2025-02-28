a84e121dc397227b4530852c7ab0b3623b68fd01
kywch <choe.kyoung@gmail.com>
2025-02-28 11:17:31 -0800

    tweak sweep

--- train_morph.py
+++ train_morph.py
@@ -296,8 +296,8 @@ def train(args, vec_env, policy, wandb=None, exp_id=None, skip_resample=False):
 
         # Mixing RMS norm reward with the raw 0-1 reward
         if not args["disable_rew_norm"]:
-            # NOTE: 0.2 hard coded for now
-            data.experience.rewards_np += 0.2 * val_norm.normalize(data.experience.rewards_np)
+            # NOTE: 0.1 hard coded for now
+            data.experience.rewards_np += 0.1 * val_norm.normalize(data.experience.rewards_np)
 
         # Update policy
         clean_pufferl.train(data)
@@ -468,12 +468,12 @@ def sweep_carbs(args, sweep_count=500, max_suggestion_cost=3600):
 
     param_spaces += [
         carbs_param("train", "learning_rate", "log", sweep_parameters, search_center=args["ssc_lr"]),
-        carbs_param('train', 'gamma', 'logit', sweep_parameters, search_center=0.92),
-        # carbs_param('train', 'gae_lambda', 'logit', sweep_parameters, search_center=0.75),
+        carbs_param('train', 'gamma', 'logit', sweep_parameters, search_center=0.97),
+        carbs_param('train', 'gae_lambda', 'logit', sweep_parameters, search_center=0.90),
         # carbs_param('train', 'update_epochs', 'linear', sweep_parameters,
         #     search_center=3, is_integer=True),
-        carbs_param("train", "clip_coef", "logit", sweep_parameters, search_center=0.05),
-        carbs_param("train", "vf_coef", "linear", sweep_parameters, search_center=1.0),
+        carbs_param("train", "clip_coef", "logit", sweep_parameters, search_center=0.03),
+        carbs_param("train", "vf_coef", "linear", sweep_parameters, search_center=0.5),
         # carbs_param("train", "vf_clip_coef", "logit", sweep_parameters, search_center=0.2),
         # carbs_param('train', 'max_grad_norm', 'linear', sweep_parameters, search_center=1.0),
         # carbs_param('train', 'ent_coef', 'log', sweep_parameters, search_center=0.0001),
@@ -571,7 +571,7 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--eval-model-path", type=str, default=None, help="Path to a pretrained checkpoint")
     parser.add_argument("--track", action="store_true", help="Track on WandB")
     parser.add_argument("--wandb-project", type=str, default="pufferlib")
-    parser.add_argument("--ssc-lr", type=float, default=.0001, help="Sweep search center for learning rate")
+    parser.add_argument("--ssc-lr", type=float, default=.0002, help="Sweep search center for learning rate")
     parser.add_argument("--ssc-rew", type=float, default=.0005, help="Sweep search center for rew power")
     parser.add_argument("--disable-rew-norm", action="store_true", help="Disable reward normalization")
 
