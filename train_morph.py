de23b7893702065d4481ff3a16265f0b554111b8
kywch <choe.kyoung@gmail.com>
2025-03-05 16:55:27 -0800

    tweak sweep

--- train_morph.py
+++ train_morph.py
@@ -451,7 +451,7 @@ def sweep_carbs(args, sweep_count=500, max_suggestion_cost=3600):
 
     param_spaces += [
         carbs_param("train", "learning_rate", "log", sweep_parameters, search_center=args["ssc_lr"]),
-        carbs_param("train", "gamma", "logit", sweep_parameters, search_center=0.97),
+        # carbs_param("train", "gamma", "logit", sweep_parameters, search_center=0.97),
         carbs_param("train", "gae_lambda", "logit", sweep_parameters, search_center=0.50),
         carbs_param('train', 'update_epochs', 'linear', sweep_parameters,
             search_center=3, is_integer=True),
