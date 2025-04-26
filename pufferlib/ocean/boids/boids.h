fa09b890bb0172890f8e58ad4379b8bca1817603
PLAZMAMA <maorioriori@gmail.com>
2025-04-26 01:47:15 -0400

    add todo

--- pufferlib/ocean/boids/boids.h
+++ pufferlib/ocean/boids/boids.h
@@ -87,6 +87,7 @@ void c_step(Boids* env, Velocity* action) {
     for (unsigned int indx = 0; indx < env->num_boids; indx++) {
         // Apply action
         Boid* current_boid = &env->boids[indx];
+        // TODO: change = to +=
         current_boid->velocity.x = action->x;
         current_boid->velocity.y = action->y;
         current_boid->x += current_boid->velocity.x;
