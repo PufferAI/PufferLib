0297261aa46a5ff940e39fe906b45cd5437afdd4
Richard Higgins <richard@relh.net>
2025-05-24 22:56:44 -0400

    metta changed their obs space this is the new one

--- pufferlib/environments/metta/torch.py
+++ pufferlib/environments/metta/torch.py
@@ -17,7 +17,7 @@ class Policy(nn.Module):
 
         self.network= nn.Sequential(
             pufferlib.pytorch.layer_init(
-                nn.Conv2d(34, cnn_channels, 5, stride=3)),
+                nn.Conv2d(26, cnn_channels, 5, stride=3)),
             nn.ReLU(),
             pufferlib.pytorch.layer_init(
                 nn.Conv2d(cnn_channels, cnn_channels, 3, stride=1)),
@@ -28,11 +28,11 @@ class Policy(nn.Module):
         )
 
         self.self_encoder = nn.Sequential(
-            pufferlib.pytorch.layer_init(nn.Linear(34, hidden_size//2)),
+            pufferlib.pytorch.layer_init(nn.Linear(26, hidden_size//2)),
             nn.ReLU(),
         )
 
-        max_vec = torch.tensor([1, 10, 30, 1, 1, 255, 100, 100, 100, 100, 100, 100, 100, 100, 1, 1, 1, 10, 1, 100, 100, 100, 100, 100, 100, 100, 100, 1, 1, 1, 1, 1, 1, 1]).float().view(1, 34, 1, 1)
+        max_vec = torch.tensor([1, 10, 30, 1, 1, 255, 100, 100, 100, 100, 100, 100, 100, 100, 1, 1, 1, 10, 1, 1, 1, 1, 1, 1, 1, 1]).float().view(1, 26, 1, 1)
         self.register_buffer('max_vec', max_vec)
 
         action_nvec = env.single_action_space.nvec
