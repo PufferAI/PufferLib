9dc328fe0e704eb7f5a74dec796e60bdd2653b53
PLAZMAMA <maorioriori@gmail.com>
2025-07-04 01:42:33 -0400

    update preset env parameters

--- pufferlib/ocean/boids/boids.h
+++ pufferlib/ocean/boids/boids.h
@@ -8,13 +8,13 @@
 
 #include "raylib.h"
 
-#define TOP_MARGIN 100
-#define BOTTOM_MARGIN 100
-#define LEFT_MARGIN 100
-#define RIGHT_MARGIN 100
+#define TOP_MARGIN 50
+#define BOTTOM_MARGIN 50
+#define LEFT_MARGIN 50
+#define RIGHT_MARGIN 50
 #define VELOCITY_CAP 5
 #define VISUAL_RANGE 400
-#define PROTECTED_RANGE 100
+#define PROTECTED_RANGE 60
 #define WIDTH 1080
 #define HEIGHT 720
 #define BOID_WIDTH 32
