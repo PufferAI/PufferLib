8d573c41618d7ca6f73c402d1f33e0e50b50599b
David Rubinstein <drubinstein@users.noreply.github.com>
2025-07-05 13:29:04 -0400

    Update pufferl.py

--- pufferlib/pufferl.py
+++ pufferlib/pufferl.py
@@ -427,8 +427,8 @@ class PuffeRL:
         y_pred = self.values.flatten()
         y_true = advantages.flatten() + self.values.flatten()
         var_y = y_true.var()
-        explained_var = torch.nan if var_y == 0 else 1 - (y_true - y_pred).var() / var_y
-        losses['explained_variance'] = explained_var.item()
+        explained_var = torch.nan if var_y == 0 else (1 - (y_true - y_pred).var() / var_y).item()
+        losses['explained_variance'] = explained_var
 
         profile.end()
         logs = None
