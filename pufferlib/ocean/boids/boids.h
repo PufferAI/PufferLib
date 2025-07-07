cc8a397b18870090f90e3f56d5225eb25a8f2e45
PLAZMAMA <maorioriori@gmail.com>
2025-07-07 00:36:54 -0400

    change reward normalization number

--- pufferlib/ocean/boids/boids.h
+++ pufferlib/ocean/boids/boids.h
@@ -197,7 +197,10 @@ void c_step(Boids *env) {
         }
 
         // Normalization
-        env->rewards[current_indx] = current_boid_reward / 2.0f;
+        // env->rewards[current_indx] = current_boid_reward;
+        env->rewards[current_indx] = current_boid_reward / 6.0f;
+        // env->rewards[current_indx] = current_boid_reward / 205.0f;
+        // env->rewards[current_indx] = current_boid_reward / 10.0f;
 
         //log updates
         if (env->tick == env->report_interval) {
