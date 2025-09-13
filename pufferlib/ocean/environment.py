7e9ca00af2fba47a337134cb0fd92184046d0c92
mmbajo <markarjaybajo@gmail.com>
2025-09-13 23:06:17 +0900

    Add Overcooked game type to environment configuration
    - Included 'overcooked' as a new game type in the MAKE_FUNCTIONS dictionary.
    - This addition expands the available game modes, enhancing the versatility of the environment.

--- pufferlib/ocean/environment.py
+++ pufferlib/ocean/environment.py
@@ -156,6 +156,7 @@ MAKE_FUNCTIONS = {
     'checkers': 'Checkers',
     'asteroids': 'Asteroids',
     'whisker_racer': 'WhiskerRacer',
+    'overcooked': 'Overcooked',
     'spaces': make_spaces,
     'multiagent': make_multiagent,
 }
