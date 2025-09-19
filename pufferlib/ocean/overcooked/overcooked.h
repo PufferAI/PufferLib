82857d8197b57f73d700e9c5c212cc87cfc4f4bc
mmbajo <markarjaybajo@gmail.com>
2025-09-19 13:58:08 +0900

    Enhance item handling in Overcooked environment
    - Added logic to track items dropped when an agent drops an item on the floor without a specific item being held, ensuring accurate logging of item interactions.
    - This change improves the tracking of gameplay events and enhances the overall performance analysis of the Overcooked environment.

--- pufferlib/ocean/overcooked/overcooked.h
+++ pufferlib/ocean/overcooked/overcooked.h
@@ -645,6 +645,10 @@ static void handle_interaction(Overcooked* env, int agent_idx) {
             }
             agent->held_item = NO_ITEM;
             env->log.items_dropped++;  // Track items dropped
+        } else if ((tile == FLOOR) && item == NULL) {
+            agent->held_item = NO_ITEM;
+            // item went to oblivion
+            env->log.items_dropped++;
         }
     }
     else {
