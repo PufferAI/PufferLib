50ea8f36cce31e52b569696f46722fc78ec2ceca
Jason Garner <elevatorguy@gmail.com>
2025-10-24 07:30:56 -0500

    needed
    # Conflicts:
    #	setup.py

--- pufferlib/pufferl.py
+++ pufferlib/pufferl.py
@@ -6,6 +6,8 @@ import contextlib
 import warnings
 warnings.filterwarnings('error', category=RuntimeWarning)
 
+import platform
+is_windows = platform.system() == "Windows"
 import os
 import io
 import sys
@@ -865,7 +867,10 @@ class Utilization(Thread):
         while not self.stopped:
             self.cpu_util.append(100*psutil.cpu_percent()/psutil.cpu_count())
             mem = psutil.virtual_memory()
-            self.cpu_mem.append(100*mem.active/mem.total)
+            if is_windows:
+                self.cpu_mem.append(100*mem.used/mem.total)
+            else:
+                self.cpu_mem.append(100*mem.active/mem.total)
             if torch.cuda.is_available():
                 # Monitoring in distributed crashes nvml
                 if torch.distributed.is_initialized():
--- setup.py
+++ setup.py
@@ -29,7 +29,14 @@ NO_TRAIN = os.getenv("NO_TRAIN", "0") == "1"
 
 # Build raylib for your platform
 RAYLIB_URL = 'https://github.com/raysan5/raylib/releases/download/5.5/'
-RAYLIB_NAME = 'raylib-5.5_macos' if platform.system() == "Darwin" else 'raylib-5.5_linux_amd64'
+system = platform.system()
+if system == 'Linux':
+    RAYLIB_NAME = 'raylib-5.5_linux_amd64'
+elif system == 'Darwin':
+    RAYLIB_NAME = 'raylib-5.5_macos'
+elif system == 'Windows':
+    RAYLIB_NAME = 'raylib-5.5_win64_msvc16'
+
 RLIGHTS_URL = 'https://raw.githubusercontent.com/raysan5/raylib/refs/heads/master/examples/shaders/rlights.h'
 
 def download_raylib(platform, ext):
@@ -113,7 +120,6 @@ else:
         '-O3',
     ]
 
-system = platform.system()
 if system == 'Linux':
     extra_compile_args += [
         '-Wno-alloc-size-larger-than',
@@ -138,6 +144,9 @@ elif system == 'Darwin':
     ]
     if not NO_OCEAN:
         download_raylib('raylib-5.5_macos', '.tar.gz')
+elif system == 'Windows':
+    if not NO_OCEAN:
+        download_raylib('raylib-5.5_win64_msvc16', '.zip')
 else:
     raise ValueError(f'Unsupported system: {system}')
 
@@ -179,7 +188,8 @@ class TorchBuildExt(cpp_extension.BuildExtension):
         self.extensions = [e for e in self.extensions if e.name in extnames]
         super().run()
 
-RAYLIB_A = f'{RAYLIB_NAME}/lib/libraylib.a'
+RAYLIB_A = f'{RAYLIB_NAME}/lib/raylibdll.lib' if system == "Windows" else f'{RAYLIB_NAME}/lib/libraylib.a'
+
 INCLUDE = [numpy.get_include(), 'raylib/include', f'{BOX2D_NAME}/include', f'{BOX2D_NAME}/src']
 extension_kwargs = dict(
     include_dirs=INCLUDE,
@@ -194,7 +204,7 @@ if not NO_OCEAN:
     c_extension_paths = glob.glob('pufferlib/ocean/**/binding.c', recursive=True)
     c_extensions = [
         Extension(
-            path.rstrip('.c').replace('/', '.'),
+            path.rstrip('.c').replace('/', '.').replace('\\', '.'),
             sources=[path],
             **extension_kwargs,
         )
@@ -236,18 +246,19 @@ if not NO_TRAIN:
         ),
     ]
 
-# Prevent Conda from injecting garbage compile flags
-from distutils.sysconfig import get_config_vars
-cfg_vars = get_config_vars()
-for key in ('CC', 'CXX', 'LDSHARED'):
-    if cfg_vars[key]:
-        cfg_vars[key] = cfg_vars[key].replace('-B /root/anaconda3/compiler_compat', '')
-        cfg_vars[key] = cfg_vars[key].replace('-pthread', '')
-        cfg_vars[key] = cfg_vars[key].replace('-fno-strict-overflow', '')
+if system != 'Windows':
+    # Prevent Conda from injecting garbage compile flags
+    from distutils.sysconfig import get_config_vars
+    cfg_vars = get_config_vars()
+    for key in ('CC', 'CXX', 'LDSHARED'):
+        if cfg_vars[key]:
+            cfg_vars[key] = cfg_vars[key].replace('-B /root/anaconda3/compiler_compat', '')
+            cfg_vars[key] = cfg_vars[key].replace('-pthread', '')
+            cfg_vars[key] = cfg_vars[key].replace('-fno-strict-overflow', '')
 
-for key, value in cfg_vars.items():
-    if value and '-fno-strict-overflow' in str(value):
-        cfg_vars[key] = value.replace('-fno-strict-overflow', '')
+    for key, value in cfg_vars.items():
+        if value and '-fno-strict-overflow' in str(value):
+            cfg_vars[key] = value.replace('-fno-strict-overflow', '')
 
 install_requires = [
     'setuptools',
