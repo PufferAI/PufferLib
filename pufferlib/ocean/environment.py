41bfb563c53ec424c3ae950bfe56a20fd7343338
Rowobin <unix@LAPTOP-247IVLS9.criticaltechworks.com>
2025-12-07 11:47:58 +0000

    Created base files for dinosaur environment.

--- pufferlib/ocean/environment.py
+++ pufferlib/ocean/environment.py
@@ -162,6 +162,7 @@ MAKE_FUNCTIONS = {
     'spaces': make_spaces,
     'multiagent': make_multiagent,
     'slimevolley': 'SlimeVolley',
+    'dinosaur': 'Dinosaur'
 }
 
 def env_creator(name='squared', *args, **kwargs):
