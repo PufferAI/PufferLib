ff89120830d0159b396e46bd81f5a8c51343689c
mmbajo <markarjaybajo@gmail.com>
2025-12-30 13:48:43 +0900

    Update weights file for coordination ring layout in Overcooked
    - Changed the weights file path for the LAYOUT_COORDINATION_RING from 'puffer_overcooked_weights_cr.bin' to 'puffer_overcooked_weights_cor.bin' to reflect the new naming convention.
    - Added the new binary weights file 'puffer_overcooked_weights_cor.bin' to support the updated layout configuration.

--- pufferlib/ocean/overcooked/overcooked.c
+++ pufferlib/ocean/overcooked/overcooked.c
@@ -21,7 +21,7 @@ int main(int argc, char** argv) {
         weights_file = "resources/overcooked/puffer_overcooked_weights_fc.bin";
         weights_size = 138631;
     } else if (layout_id == LAYOUT_COORDINATION_RING) {
-        weights_file = "resources/overcooked/puffer_overcooked_weights_cr.bin";
+        weights_file = "resources/overcooked/puffer_overcooked_weights_cor.bin";
         weights_size = 138631;
     } else {
         weights_file = "resources/overcooked/puffer_overcooked_weights_cr.bin";
